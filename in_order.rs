7e321f077792189b76c1813105fd3e9f39335199
KaRaKurT1 <81345256+KaRaKurT1@users.noreply.github.com>
2023-10-08 04:23:19 +0500

    Update in_order.rs

--- in_order.rs
+++ in_order.rs
@@ -15,11 +15,14 @@ impl<T> TreeNode<T> {
     }
 }
 
-pub fn in_order_traversal<T: std::fmt::Debug>(node: &Option<Box<TreeNode<T>>>) {
+pub fn in_order_traversal<T>(node: &Option<Box<TreeNode<T>>>, result: &mut Vec<T>) 
+where
+    T: Clone + std::fmt::Debug,
+{
     if let Some(inner_node) = node {
         let node_ref = &*inner_node;
-        in_order_traversal(&node_ref.left);
-        println!("{:? }", node_ref.value);
-        in_order_traversal(&node_ref.right);
+        in_order_traversal(&node_ref.left, result);
+        print!("{:?} ", node_ref.value);
+        in_order_traversal(&node_ref.right, result);
     }
 }
